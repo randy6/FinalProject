9998e3cd474215b398964d207ee7929d24a77e53
Jordon Hankins <jordon@jordons-mini.dhcp.unc.edu>
2019-12-03 17:06:14 -0500

    Made new branch for UI -Jordon

--- render.js
+++ render.js
@@ -1,1 +1,31 @@
 /* This is where the functionality will take place. */
+
+/* Render the Board */
+export const loadBoard = function () {
+    return `<div class="container">
+                <div class="row">
+                    <div id="Planner" class="col">
+                        <div>
+                            <h1> Test </h1>
+                        </div>
+                    </div>
+                    <div id="Accounts" class="col">
+                        <div>
+                            <h1> Test2 </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+};
+
+/* Render All */
+export const loadAll = function () {
+    const $screen = $('#screen');
+
+    $screen.append(loadBoard());
+};
+
+/* Execute loadAll */
+$(function () {
+    loadAll();
+});
