166d2a6f5806cb22b43346d7a186747a95777b89
Jordon Hankins <jordon@jordons-mini.dhcp.unc.edu>
2019-12-03 18:30:50 -0500

    Modifications -Jordon

--- render.js
+++ render.js
@@ -2,16 +2,41 @@
 
 /* Render the Board */
 export const loadBoard = function () {
-    return `<div class="container">
-                <div class="row">
-                    <div id="Planner" class="col">
-                        <div>
-                            <h1> Test </h1>
+    return `<div id="Planner-Side" class="split left">
+                <div id="Days-Of-Week" class="container">
+                    <div class="row">
+                        <div id="Sunday" class="col">
+                            <h1> Sunday </h1>
+                        </div>
+                        <div id="Monday" class="col">
+                            <h1> Monday </h1>
+                        </div>
+                        <div id="Tuesday" class="col">
+                            <h1> Tuesday </h1>
+                        </div>
+                        <div id="Wednesday" class="col">
+                            <h1> Wednesday </h1>
+                        </div>
+                        <div id="Thursday" class="col">
+                            <h1> Thursday </h1>
+                        </div>
+                        <div id="Friday" class="col">
+                            <h1> Friday </h1>
+                        </div>
+                        <div id="Saturday" class="col">
+                            <h1> Saturday </h1>
                         </div>
                     </div>
-                    <div id="Accounts" class="col">
-                        <div>
-                            <h1> Test2 </h2>
+                </div>
+            </div>
+            <div id="Account-Side" class="split right">
+                <div id="Buttons" class="container">
+                    <div class="col">
+                        <div id="Logins" class="row">
+                            <h1> Login Button </h1>
+                        </div>
+                        <div id="Theme" class="row">
+                            <h1> Theme Button </h1>
                         </div>
                     </div>
                 </div>
