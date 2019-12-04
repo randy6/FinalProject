1d439287a64af6601fe3c81c51f9d35627f5c790
Jordon Hankins <jordon@jordons-mini.dhcp.unc.edu>
2019-12-03 20:01:28 -0500

    Updated UI and added some basic function setups. -Jordon

--- render.js
+++ render.js
@@ -3,62 +3,64 @@
 /* Render the Board */
 export const loadBoard = function () {
     return `<div id="Planner-Side" class="split left">
-                <div id="Days-Of-Week" class="container">
-                    <div class="row">
-                        <div id="Monday" class="col">
-                            <div id="Title">
-                                <h1> Monday </h1>
-                            </div>
-                            <div id="todo">
-
-                            </div>
-                            <div id="button">
-                                <button type="button" id="edit"> Edit </button>
-                            </div>
+                <div class="row">
+                    <div id="Monday" class="col">
+                        <div id="Title">
+                            <h1> Monday </h1>
                         </div>
-                        <div id="Tuesday" class="col">
-                            <div id="Title">
-                                <h1> Tuesday </h1>
-                            </div>
-                            <div id="todo">
-
-                            </div>
-                            <div id="button">
-                                <button type="button" id="edit"> Edit </button>
-                            </div>
+                        <div id="todo">
+                            <ul id="list">
+                                <li id="element"> 
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </li>
+                            </ul>
                         </div>
-                        <div id="Wednesday" class="col">
-                            <div id="Title">
-                                <h1> Wednesday </h1>
-                            </div>
-                            <div id="todo">
-
-                            </div>
-                            <div id="button">
-                                <button type="button" id="edit"> Edit </button>
-                            </div>
+                        <div id="button">
+                            <button type="submit" id="add" onclick="addToListPress(event)"> Add To List </button>
                         </div>
-                        <div id="Thursday" class="col">
-                            <div id="Title">
-                                <h1> Thursday </h1>
-                            </div>
-                            <div id="todo">
-
-                            </div>
-                            <div id="button">
-                                <button type="button" id="edit"> Edit </button>
-                            </div>
+                    </div>
+                    <div id="Tuesday" class="col">
+                        <div id="Title">
+                            <h1> Tuesday </h1>
                         </div>
-                        <div id="Friday" class="col">
-                            <div id="Title">
-                                <h1> Friday </h1>
-                            </div>
-                            <div id="todo">
-
-                            </div>
-                            <div id="button">
-                                <button type="button" id="edit"> Edit </button>
-                            </div>
+                        <div id="todo">
+                            <ul id="list"> </ul>
+                        </div>
+                        <div id="button">
+                            <button type="submit" id="add" onclick="addToListPress(event)"> Add To List </button>
+                        </div>
+                    </div>
+                    <div id="Wednesday" class="col">
+                        <div id="Title">
+                            <h1> Wednesday </h1>
+                        </div>
+                        <div id="todo">
+                            <ul id="list"> </ul>
+                        </div>
+                        <div id="button">
+                            <button type="submit" id="add" onclick="addToListPress(event)"> Add To List </button>
+                        </div>
+                    </div>
+                    <div id="Thursday" class="col">
+                        <div id="Title">
+                            <h1> Thursday </h1>
+                        </div>
+                        <div id="todo">
+                            <ul id="list"> </ul>
+                        </div>
+                        <div id="button">
+                            <button type="submit" id="add" onclick="addToListPress(event)"> Add To List </button>
+                        </div>
+                    </div>
+                    <div id="Friday" class="col">
+                        <div id="Title">
+                            <h1> Friday </h1>
+                        </div>
+                        <div id="todo">
+                            <ul id="list"> </ul>
+                        </div>
+                        <div id="button">
+                            <button type="submit" id="add" onclick="addToListPress(event)"> Add To List </button>
                         </div>
                     </div>
                 </div>
@@ -67,29 +69,48 @@ export const loadBoard = function () {
                 <div id="Buttons" class="container">
                     <div class="col">
                         <div id="Logins" class="row">
-                            <button type="button"> Login </button>
-                            <button type="button"> Sign Up </button>
+                            <button type="submit" id="login" onclick="login(event)"> Login </button>
+                            <button type="submit" id="signup" onclick="signUp(event)"> Sign Up </button>
                         </div>
                         <br>
                         <div id="Theme" class="row">
-                            <button type="button"> Theme Select </button>
+                            <button type="submit" id="theme" onclick="themeSelect(event)"> Theme Select </button>
                         </div>
                     </div>
                 </div>
             </div>`;
 };
 
-/* Handle Edit Button Press */
+/* Handle Add To List Button Press */
+export const addToListPress = function (event) {
+    alert("Add to List was clicked");
+};
 
-/* Handle Save Button Press */
+/* Handle Login Button Press */
+export const login = function (event) {
+    alert("Login Button was clicked");
+};
 
-/* Handle Cancel Button Press */
+/* Handle Sign Up Button Press */
+export const signUp = function (event) {
+    alert("Sign Up Button was clicked");
+};
+
+/* Handle Theme Selector Press */
+export const themeSelect = function (event) {
+    alert("Theme Select Button was clicked");
+};
 
 /* Render All */
 export const loadAll = function () {
     const $screen = $('#screen');
 
     $screen.append(loadBoard());
+
+    $("#screen").on("click", "#add", addToListPress);
+    $("#screen").on("click", "#login", login);
+    $("#screen").on("click", "#signup", signUp);
+    $("#screen").on("click", "#theme", themeSelect);
 };
 
 /* Execute loadAll */
