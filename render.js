dab5289a83c1d485a502352dbc000a05f01f4788
Randy Le <randyle@live.unc.edu>
2019-12-08 18:17:40 -0500

    New UI -randy

--- render.js
+++ render.js
@@ -1,119 +1,70 @@
-/* This is where the functionality will take place. */
+function addItem() {
+	let item = document.getElementById('task').value;
+	let day = document.getElementById('days').value;
+	let newItem = document.createElement('li');
+	/*
+		This is an adaption of the to-do-list I made a couple of days ago. I wanted to make the checkboxes disappear and only appear when hovered upon, but i was
+		unable to get it to work. I got it to work for one but when you hover over the other items it would show the first items checkbox and not its checkbox. Still working the bugs out on that part. Hope you guys like the planner.
+		
+		Any suggestions for improvements is welcome :)
+	*/
+	
+	let checkbox = document.createElement('input');
+	let newLine = document.createElement('br');
+	newItem.setAttribute('class','list-item')
+	checkbox.setAttribute('type','checkbox');
+	checkbox.setAttribute('id','check-box');
+	newItem.textContent = item;
+	if (day == 'monday') {
+		document.getElementById('monday').appendChild(newItem);
+		document.getElementById('monday').appendChild(checkbox);
+		document.getElementById('monday').appendChild(newLine);
+	} else if (day == 'tuesday'){
+		document.getElementById('tuesday').appendChild(newItem);
+		document.getElementById('tuesday').appendChild(checkbox);
+		document.getElementById('tuesday').appendChild(newLine);
+	}else if (day == 'wednesday') {
+		document.getElementById('wednesday').appendChild(newItem);
+		document.getElementById('wednesday').appendChild(checkbox);
+		document.getElementById('wednesday').appendChild(newLine);
+	} else if (day == 'thursday') {
+		document.getElementById('thursday').appendChild(newItem);
+		document.getElementById('thursday').appendChild(checkbox);
+		document.getElementById('thursday').appendChild(newLine);
+	} else if (day == 'friday') {
+		document.getElementById('friday').appendChild(newItem);
+		document.getElementById('friday').appendChild(checkbox);
+		document.getElementById('friday').appendChild(newLine);
+	} else if (day == 'saturday') {
+		document.getElementById('saturday').appendChild(newItem);
+		document.getElementById('saturday').appendChild(checkbox);
+		document.getElementById('saturday').appendChild(newLine);
+	} else if (day == 'sunday') {
+		document.getElementById('sunday').appendChild(newItem);
+		document.getElementById('sunday').appendChild(checkbox);
+		document.getElementById('sunday').appendChild(newLine);
+	}
+	document.getElementById('task').value = "";
+}
 
-/* Render the Board */
-export const loadBoard = function () {
-    return `<div id="Planner-Side" class="split left">
-                <div class="row">
-                    <div id="Monday" class="col">
-                        <div id="Title">
-                            <h1> Monday </h1>
-                        </div>
-                        <div id="todo">
-                            <ul id="list">
-                                <li id="element"> 
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                </li>
-                            </ul>
-                        </div>
-                        <div id="button">
-                            <button type="submit" id="add" onclick="addToListPress(event)"> Add To List </button>
-                        </div>
-                    </div>
-                    <div id="Tuesday" class="col">
-                        <div id="Title">
-                            <h1> Tuesday </h1>
-                        </div>
-                        <div id="todo">
-                            <ul id="list"> </ul>
-                        </div>
-                        <div id="button">
-                            <button type="submit" id="add" onclick="addToListPress(event)"> Add To List </button>
-                        </div>
-                    </div>
-                    <div id="Wednesday" class="col">
-                        <div id="Title">
-                            <h1> Wednesday </h1>
-                        </div>
-                        <div id="todo">
-                            <ul id="list"> </ul>
-                        </div>
-                        <div id="button">
-                            <button type="submit" id="add" onclick="addToListPress(event)"> Add To List </button>
-                        </div>
-                    </div>
-                    <div id="Thursday" class="col">
-                        <div id="Title">
-                            <h1> Thursday </h1>
-                        </div>
-                        <div id="todo">
-                            <ul id="list"> </ul>
-                        </div>
-                        <div id="button">
-                            <button type="submit" id="add" onclick="addToListPress(event)"> Add To List </button>
-                        </div>
-                    </div>
-                    <div id="Friday" class="col">
-                        <div id="Title">
-                            <h1> Friday </h1>
-                        </div>
-                        <div id="todo">
-                            <ul id="list"> </ul>
-                        </div>
-                        <div id="button">
-                            <button type="submit" id="add" onclick="addToListPress(event)"> Add To List </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id="Account-Side" class="split right">
-                <div id="Buttons" class="container">
-                    <div class="col">
-                        <div id="Logins" class="row">
-                            <button type="submit" id="login" onclick="login(event)"> Login </button>
-                            <button type="submit" id="signup" onclick="signUp(event)"> Sign Up </button>
-                        </div>
-                        <br>
-                        <div id="Theme" class="row">
-                            <button type="submit" id="theme" onclick="themeSelect(event)"> Theme Select </button>
-                        </div>
-                    </div>
-                </div>
-            </div>`;
-};
-
-/* Handle Add To List Button Press */
-export const addToListPress = function (event) {
-    alert("Add to List was clicked");
-};
-
-/* Handle Login Button Press */
-export const login = function (event) {
-    alert("Login Button was clicked");
-};
-
-/* Handle Sign Up Button Press */
-export const signUp = function (event) {
-    alert("Sign Up Button was clicked");
-};
-
-/* Handle Theme Selector Press */
-export const themeSelect = function (event) {
-    alert("Theme Select Button was clicked");
-};
-
-/* Render All */
-export const loadAll = function () {
-    const $screen = $('#screen');
-
-    $screen.append(loadBoard());
-
-    $("#screen").on("click", "#add", addToListPress);
-    $("#screen").on("click", "#login", login);
-    $("#screen").on("click", "#signup", signUp);
-    $("#screen").on("click", "#theme", themeSelect);
-};
-
-/* Execute loadAll */
-$(function () {
-    loadAll();
-});
+function popOffItem() {
+	let checked_list = document.querySelectorAll('#check-box');
+	let checked_list_item = document.querySelectorAll('.list-item');
+	let lineBreaks = document.querySelectorAll('br');
+	for (i=0; i < checked_list.length; i++) {
+		if (checked_list[i].checked==true) {	  
+			//Removing the Item from the Document
+			let checkbox_elemt = checked_list[i];
+			let item = checked_list_item[i];
+			let removeBreakLine = lineBreaks[i];
+			let parent1 = checkbox_elemt.parentNode;
+			let parent2 = item.parentNode;
+			let parent3 = removeBreakLine.parentNode;
+			parent1.removeChild(checkbox_elemt);
+			parent2.removeChild(item);
+			parent3.removeChild(removeBreakLine);
+		}else {
+			continue;
+		}
+	}
+}
