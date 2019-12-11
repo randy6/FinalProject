6a799adf47824c77fb3ff43179306efec04259a9
Jordon Hankins <jordon@jordons-mini.dhcp.unc.edu>
2019-12-10 21:36:19 -0500

    Added the apis

--- render.js
+++ render.js
@@ -8,22 +8,22 @@ function addItem() {
 		
 		Any suggestions for improvements is welcome :)
 	*/
-	
+
 	let checkbox = document.createElement('input');
 	let newLine = document.createElement('br');
-	newItem.setAttribute('class','list-item')
-	checkbox.setAttribute('type','checkbox');
-	checkbox.setAttribute('id','check-box');
+	newItem.setAttribute('class', 'list-item')
+	checkbox.setAttribute('type', 'checkbox');
+	checkbox.setAttribute('id', 'check-box');
 	newItem.textContent = item;
 	if (day == 'monday') {
 		document.getElementById('monday').appendChild(newItem);
 		document.getElementById('monday').appendChild(checkbox);
 		document.getElementById('monday').appendChild(newLine);
-	} else if (day == 'tuesday'){
+	} else if (day == 'tuesday') {
 		document.getElementById('tuesday').appendChild(newItem);
 		document.getElementById('tuesday').appendChild(checkbox);
 		document.getElementById('tuesday').appendChild(newLine);
-	}else if (day == 'wednesday') {
+	} else if (day == 'wednesday') {
 		document.getElementById('wednesday').appendChild(newItem);
 		document.getElementById('wednesday').appendChild(checkbox);
 		document.getElementById('wednesday').appendChild(newLine);
@@ -51,8 +51,8 @@ function popOffItem() {
 	let checked_list = document.querySelectorAll('#check-box');
 	let checked_list_item = document.querySelectorAll('.list-item');
 	let lineBreaks = document.querySelectorAll('br');
-	for (i=0; i < checked_list.length; i++) {
-		if (checked_list[i].checked==true) {	  
+	for (i = 0; i < checked_list.length; i++) {
+		if (checked_list[i].checked == true) {
 			//Removing the Item from the Document
 			let checkbox_elemt = checked_list[i];
 			let item = checked_list_item[i];
@@ -63,8 +63,22 @@ function popOffItem() {
 			parent1.removeChild(checkbox_elemt);
 			parent2.removeChild(item);
 			parent3.removeChild(removeBreakLine);
-		}else {
+		} else {
 			continue;
 		}
 	}
 }
+
+
+
+
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=CHAPEL HILL&units=imperial&APPID=bf1d771756fdc7d1248bad881f19f13f", function (data) {
+	console.log(data);
+	// http://openweathermap.org/img/q/04n.png
+	let icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+	let temp = Math.floor(data.main.temp);
+	let weather = data.weather[0].main;
+	$('.icon').attr('src', icon);
+	$('.weather').append(weather);
+	$('.temperature').append(temp);
+});
